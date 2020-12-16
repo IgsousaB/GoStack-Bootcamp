6eda3facfbabc6568e3b6dae538b99aa38bd9347
Igor Baldoino <igortthq@gmail.com>
2020-12-16 18:02:53 -0300

    2.9 - Middlewares

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // IMPORTS 
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 // APP SETUP
 const app = express();
@@ -8,4 +9,69 @@ app.use(express.json());
 // DECLARATIONS
 const projects = [];
 
+function logRequests(request, response, next) {
+   const { method, url } = request;
+   const logLabel = `[${method.toUpperCase()} ${url}]`;
+   
+   console.time(logLabel);
+   next();
+   console.timeEnd(logLabel);
+}
+
+app.use(logRequests);
+
 // ROUTES
+app.get('/projects', (request, response) => {
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+  return response.json(results);
+});
+
+
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+  const project = { id: uuid(), title, owner };
+  
+  projects.push(project);
+  
+  return response.json(project);
+});
+
+
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+  const projectIndex = projects.findIndex(project => project.id === id);
+  
+  if (projectIndex < 0){    
+    return response.status(404).json({ error: 'Project not found' });
+  }
+  const project = { 
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+  return response.json(project);
+});
+
+
+app.delete('/projects/:id', (request, response) => {
+  const {id} = request.params;
+  const projectIndex = projects.findIndex(project => project.id === id);
+  
+  if (projectIndex < 0){
+    return response.status(404).json({ error: 'Projects not found.' });
+  }
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send();
+});
+
+
+app.listen(3333, () => {console.log('🚀 Back-end started!')});
